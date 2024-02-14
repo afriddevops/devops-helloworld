040fa1877fc59137484d4850af65393c36db29b9
chinna421 <108382623+chinna421@users.noreply.github.com>
2024-02-14 11:50:03 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,6 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
-echo "this is webhook project"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
