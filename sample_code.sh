b76c1ec3b6d8ee455599084598bf2dc1116cc666
chinna421 <108382623+chinna421@users.noreply.github.com>
2024-02-14 11:52:52 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
-echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
