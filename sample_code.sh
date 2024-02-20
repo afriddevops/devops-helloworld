745da82aee0f168e931cde621eec2ef0cbb391ba
afriddevops <134026231+afriddevops@users.noreply.github.com>
2024-02-20 15:47:31 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
+echo "This is my test"
 echo "this is test from redhat machine"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
